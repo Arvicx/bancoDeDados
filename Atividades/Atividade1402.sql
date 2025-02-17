0c57cca427c01f74c801dfa8c7ffe7d07ea296ac
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-02-17 17:07:24 -0300

    Update Atividade1402.sql

--- Atividades/Atividade1402.sql
+++ Atividades/Atividade1402.sql
@@ -18,4 +18,28 @@ create table livros(
 );
 
 -- C
-INSERT INTO autores VALUES ("Arthur", "Brasileiro");
+INSERT INTO autores VALUES (1,"Arthur", "Brasileiro");
+INSERT INTO autores(nome,nacionalidade) VALUES ("Wildiane", "Grega");
+INSERT INTO autores(nome,nacionalidade) VALUES ("Giovanna", "Turca");
+
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("A Vida é Estranha", 2015, 1);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("Antes da Tempestade", 2019, 1);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("Meu Romance Químico", 2004, 2);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("A Parada Fúnebre", 2006, 2);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("MC Daleste: A Biografia", 2012, 3);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("Dome Empadas", 2020, 3);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("Dias Perigosos", 2013, 1);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("Amantes da Destruição", 2012, 2);
+INSERT INTO livros(titulo, ano_publicacao, id_autor) VALUES ("Metamorfose", 2019, 1);
+
+-- D
+
+SELECT * FROM autores;
+SELECT * FROM livros;
+SELECT livros.titulo , autores.nome FROM livros JOIN autores ON livros.id_autor = autores.id;
+SELECT livros.titulo, autores.nome FROM livros JOIN autores ON livros.id_autor = autores.id WHERE autores.nome = "Arthur";
+SELECT livros.titulo, autores.nome FROM livros JOIN autores ON livros.id_autor = autores.id WHERE autores.nome = "Wildiane";
+SELECT livros.titulo, autores.nome FROM livros JOIN autores ON livros.id_autor = autores.id WHERE autores.nome = "Giovanna";
+SELECT autores.nome, COUNT(*) FROM autores JOIN livros ON autores.id = livros.id_autor WHERE autores.nome = "Arthur";
+SELECT autores.nome, COUNT(*) FROM autores JOIN livros ON autores.id = livros.id_autor WHERE autores.nome = "Wildiane";
+SELECT autores.nome, COUNT(*) FROM autores JOIN livros ON autores.id = livros.id_autor WHERE autores.nome = "Giovanna";
